f53a302a49bb61042214b7a36fde79069ca72407
Kazuhiro Kubota <k2.wanko@gmail.com>
2016-12-13 00:50:42 +0900

    Update appengine/basic

--- appengine/basic/server.go
+++ appengine/basic/server.go
@@ -3,16 +3,22 @@ package main
 import (
 	"net/http"
 
+	"google.golang.org/appengine"
+
+	"fmt"
+
 	"github.com/k2wanko/horo"
+	"github.com/k2wanko/horo/middleware"
 	"golang.org/x/net/context"
 )
 
 func init() {
 	h := horo.New()
+	h.Use(middleware.Recover(), middleware.AppContext())
 	h.GET("/", handler)
 	http.Handle("/", h)
 }
 
 func handler(c context.Context) error {
-	return horo.Text(c, http.StatusOK, "Hello World!")
+	return horo.Text(c, http.StatusOK, fmt.Sprintf("Hello, %s", appengine.AppID(c)))
 }
